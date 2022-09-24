79bdd9fcf73d4a895436072e2f3bd7cd6f0a8588
Prajwal Bati <prajwal.bati@ekbana.info>
2022-09-24 17:03:00 +0545

    game start and submit guess number

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,91 @@
 import Head from 'next/head'
+import {useState} from 'react';
 
 export default function Home() {
+
+  const [game, setGame] = useState({});
+  const [round, setRound] = useState(1);
+  const [guessNumber, setGuessNumber] = useState('');
+  const [numberError, setNumberError] = useState('');
+
+  const changeHandler = (e) => {
+    let enteredNumber = e.target.value;
+    if(Number(enteredNumber) > 0 && Number(enteredNumber) < 10) {
+      setGuessNumber(Number(enteredNumber));
+    } else {
+      setNumberError('Number must be between 0 to 9.99');
+    }
+  }
+
+  const submitGuessNumber = ()=> {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/${game.gameId}/submit-guess/${game.roundId}`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+        'guessedNumber': guessNumber
+      })
+    })
+    .then(function(response){
+      return response.json()})
+    .then(function(data) {
+      console.log(data);
+    }).catch(error => console.error('Error:', error));
+  }
+
+  const startGame = async() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/start-game`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+    })
+    .then(function(response){
+      return response.json()})
+    .then(function(data) {
+      console.log(data);
+      setGame(data.gameDetails);
+    }).catch(error => console.error('Error:', error));
+  };
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Guess Game</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Guess Game
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+        {game?.gameId ? (
+          <>
+            <h3>Round {round}</h3>
+            <p className="description">
+              Enter your guess number
             </p>
-          </a>
-        </div>
+            <div>
+              <input type="number" className="form-control" onChange={changeHandler} /> <br />
+              <button className="btn btn-primary" onClick={submitGuessNumber}>Submit</button>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className="description">
+              Click Start Game Button to start game
+            </p>
+            <div>
+              <button className="btn btn-primary" onClick={startGame}>Start Game</button>
+            </div>
+          </>
+        )}
+
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
+
 
       <style jsx>{`
         .container {
